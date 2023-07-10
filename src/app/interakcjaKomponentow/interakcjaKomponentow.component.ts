4dc31bb537a4cdae924d8d49f9d334c9eb94bb91
kacper0276 <72521677+kacper0276@users.noreply.github.com>
2023-07-11 01:54:44 +0200

    Update interakcjaKomponentow.component.ts

--- src/app/interakcjaKomponentow/interakcjaKomponentow.component.ts
+++ src/app/interakcjaKomponentow/interakcjaKomponentow.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-naukainterakcji',
   templateUrl: './interakcjaKomponentow.component.html',
 })
-export class InterakcjaKomponentow {
-  constructor() {}
+export class InterakcjaKomponentow implements OnInit {
+  user: { id: string };
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.user = {
+      id: this.route.snapshot.params['id'],
+    };
+    this.route.params.subscribe((data) => {
+      this.user = {
+        id: data['id'],
+      };
+
+      console.log(this.user);
+    });
+
+    console.log(this.user);
+  }
+
 
   tasks = ['Tak', 'Nie', 'Cos'];
 
